a775591639d2932f9370c429c9889f21e983b309
gengzi <1164014750@qq.com>
2018-11-20 11:33:36 +0800

    spring boot 提供的监控 actuator

---
+++ springboot-actuator/src/main/java/club/gsjblog/actuator/ActuatorApplication.java
@@ -0,0 +1,17 @@
+package club.gsjblog.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2018/11/20.
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+
+}
