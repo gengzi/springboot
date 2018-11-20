581477dbd5fc9a0980ec2e30c19389a6e7d15955
gengzi <1164014750@qq.com>
2018-11-20 17:34:13 +0800

    resttemplate

--- ribben-consumer/src/main/java/club/gsjblog/ribben/consumer/controller/ConsumerController.java
+++ ribben-consumer/src/main/java/club/gsjblog/ribben/consumer/controller/ConsumerController.java
@@ -1,9 +1,12 @@
 package club.gsjblog.ribben.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 /**
  * Created by Administrator on 2018/11/19.
@@ -19,6 +22,61 @@ public class ConsumerController {
     public String helloConsumer(){
 
         return restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
+    }
+
+
+    @RequestMapping(value = "/ribben-consumer/get")
+    public String helloConsumer(){
+
+        /**
+         * 第一种  getforentity
+         *
+         * url   请求的地址
+         * responseType  请求响应体body 的包装类型
+         * urlVariables   url参数绑定
+         */
+//        ResponseEntity<String> didi = restTemplate.getForEntity("http://USER-SERVICE/user?name={1}", String.class, "didi");
+//        String body = didi.getBody();
+//        return body;
+
+
+
+        /**
+         * 第二种  getforentity
+         *
+         * url   请求的地址
+         * responseType  请求响应体body 的包装类型
+         * urlVariables   url参数绑定  参数为map
+         */
+//        HashMap<String, String> map = new HashMap<String, String>();
+//        map.put("name","didi");
+//        restTemplate.getForEntity("http://USER-SERVICE/user?name={name}", String.class,map);
+//        String body = didi.getBody();
+//        return body;
+
+
+        /**
+         * 第二种  getforentity
+         *
+         * url   请求的地址
+         * responseType  请求响应体body 的包装类型
+         * urlVariables   url参数绑定  参数为map
+         */
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("name","didi");
+        restTemplate.getForEntity("http://USER-SERVICE/user?name={name}", String.class,map);
+        String body = didi.getBody();
+        return body;
+
+
+
+
+
+
+
+
+
+//        return restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
     }
 
 
